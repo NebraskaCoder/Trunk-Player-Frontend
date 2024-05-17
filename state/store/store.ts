13c15d244b183bad96487052ef0e28e186c77f2c
Gabriel Graves <nebraskacoder@gmail.com>
2024-05-17 10:19:48 +0000

    refactor: Remove appNotificationsSlice from store configuration

--- state/store/store.ts
+++ state/store/store.ts
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import layoutReducer from "@/state/slices/layoutSlice";
-import appNotificationsReducer from "@/state/slices/appNotificationsSlice";
 
 export function makeStore() {
   return configureStore({
     reducer: {
       layout: layoutReducer,
-      appnotifications: appNotificationsReducer,
     },
     devTools: process.env.NODE_ENV !== "production",
   });
